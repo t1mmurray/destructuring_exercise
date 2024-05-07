214ffd80eecdd4e8faab0f5e9c3c26adf0966ef7
Tim <tjm6014@gmail.com>
2024-05-07 13:00:23 -0700

    initial

---
+++ app.js
@@ -0,0 +1,58 @@
+/*// 1.
+
+8;
+1846;
+
+// 2.
+
+discoveryYears = {
+  yearNeptuneDiscoverd: 1846,
+  yearMarsDiscovered: 1659,
+};
+
+// 3.
+// a.
+"Your name is Alejandro and you like purple";
+// b.
+"Your name is Melissa and you like green";
+//c.
+"Your name is undefined and you like green";
+
+//4.
+//a.
+"Maya";
+//b.
+"Marisa";
+//c.
+"Chi";
+
+//5.
+//a.
+"Raindrops on roses";
+//b.
+"whiskers on kittens"
+//c.
+["Bright copper kettles",
+"warm woolen mittens",
+"Brown paper packages tied up with strings"]
+//6.
+[10, 30, 20];
+
+//7.
+var { numbers: { a, b }} = obj;
+
+//8:
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+//9:
+
+const raceResults = ([first, second, third, ...rest]) => {
+    return {
+        first,
+        second,
+        third,
+        rest
+    }
+}
+
+*/
